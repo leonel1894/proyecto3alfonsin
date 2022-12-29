8574fda8888084e6e4fa950e8bd9d07a49614a6a
leonel alfonsin <leonelalfonsin1894@gmail.com>
2022-12-28 23:35:12 -0300

    agregue una funcion

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-let importe = 250;
-for (let i = 1; i <= 3; i++) {
-    let cantidad = prompt("Ingrese la cantidad de empanadas que quiera.");
-    let intento = i + " " + "de 3"
-    if (cantidad >= 6 && cantidad <= 24) {
-        alert("El pedido fue realizado con exito, el total a pagar es de $" + importe * cantidad);
-        break;
-    }
-    else {
-        alert("La cantidad ingresada no es correcta, por favor vuelva a realizar la operacion. " + intento);
+function solicitudEmpanadas() {
+    let importe = 250;
+    for (let i = 1; i <= 3; i++) {
+        let cantidad = prompt("Ingrese la cantidad de empanadas que quiera.");
+        let intento = i + " " + "de 3"
+        if (cantidad >= 6 && cantidad <= 24) {
+            alert("El pedido fue realizado con exito, el total a pagar es de $" + importe * cantidad);
+            break;
+        }
+        else {
+            alert("La cantidad ingresada no es correcta, por favor vuelva a realizar la operacion, minimo 6 y maximo 24 empanadas. Intentos" + " " + intento);
+        }
     }
 }
+
+solicitudEmpanadas();
